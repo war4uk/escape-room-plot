b8923b667d6ed6c0f291955567e167b220635849
Alexander Polyakov <war4uk@gmail.com>
2016-10-10 10:05:14 +0300

    reverse charts

--- wwwroot/app/generate-chart-data.js
+++ wwwroot/app/generate-chart-data.js
@@ -4,13 +4,8 @@ export default responseData => {
     const names = ["шизофрения", "зомби лаборатория"];
     const barWidth = 30;
 
-    const shizaPayments = [names[0], ...responseData.map(payment => payment.aggregatedRoomPayments[names[0]])];
-    const zombiePayments = [names[1], ...responseData.map(payment => payment.aggregatedRoomPayments[names[1]])];
-
-    const xValues = ['x', ...responseData.map(payment => moment(payment.date).format('MMM DD'))];
-    
-    const sizeSettings = { 
-        width: 2 * shizaPayments.length * (barWidth + 5)
+    const sizeSettings = {
+        width: 2 * (responseData.length + 1) * (barWidth + 5)
     };
     const axis = {
         x: {
@@ -24,6 +19,10 @@ export default responseData => {
 
     let types = {};
     names.forEach(name => { types[name] = 'bar' });
+
+    const shizaPayments = [names[0], ...responseData.map(payment => payment.aggregatedRoomPayments[names[0]]).reverse()];
+    const zombiePayments = [names[1], ...responseData.map(payment => payment.aggregatedRoomPayments[names[1]]).reverse()];
+    const xValues = ['x', ...responseData.map(payment => moment(payment.date).format('MMM DD')).reverse()];
 
     const data = {
         x: 'x',
